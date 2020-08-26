2e56f38b54b76406447f0bebfbe51825cc2383e5
Bjarki Eldon <beldon11@gmail.com>
2020-08-26 14:20:19 +0200

    C++ header file for constants
    defines constants for  computing  summary statistics  of  genomic site-frequency spectrum, i.e.  a set of site-frequency spectra 
    from  unlinked loci

---
+++ xibetagenomiconstants.hpp
@@ -0,0 +1,10 @@
+/* use with  xibetagenomic.cpp */
+extern const unsigned samplesize ;
+extern const unsigned lgroups ;
+extern const unsigned Imin ;
+extern const unsigned Imax ;
+extern const unsigned Jmin ;
+extern const unsigned Jmax ;
+extern const unsigned Kmin ;
+extern const unsigned Kmax ;
+extern const unsigned trials ;
